f2edc434005c628a035c998b6d52982057da78dc
Arthur <pereiras.oarthur@gmail.com>
2023-09-14 21:38:32 -0300

    Primeiro Commit

---
+++ src/main/java/com/devarthursilva/junit5mokito/Junit5MokitoApplication.java
@@ -0,0 +1,13 @@
+package com.devarthursilva.junit5mokito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5MokitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5MokitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devarthursilva/junit5mokito/Junit5MokitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devarthursilva.junit5mokito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5MokitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
