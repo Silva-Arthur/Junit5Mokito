9dc965a61f7612266ef031bdd60f4c37472e245c
Arthur <pereiras.oarthur@gmail.com>
2023-09-21 19:14:24 -0300

    Teste para Classe UserResource método findById

---
+++ src/test/java/com/devarthursilva/junit5mokito/resources/UserResourceTest.java
@@ -0,0 +1,93 @@
+package com.devarthursilva.junit5mokito.resources;
+
+import com.devarthursilva.junit5mokito.domain.Usuario;
+import com.devarthursilva.junit5mokito.domain.dto.UsuarioDTO;
+import com.devarthursilva.junit5mokito.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserResourceTest {
+
+    public static final Integer ID = 1;
+    public static final String NAME = "Arthur Silva";
+    public static final String EMAIL = "arthur@mail.com";
+    public static final String PASSWORD = "123";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    public static final int INDEX = 0;
+    public static final String E_MAIL_JA_CADASTRADO = "E-mail já cadastrado!";
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private Usuario user;
+    private UsuarioDTO userDTO;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnSuccess() {
+        when(
+            service.findById(anyInt())
+        ).thenReturn(user);
+
+        when(
+            mapper.map(any(), any())
+        ).thenReturn(userDTO);
+
+        ResponseEntity<UsuarioDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UsuarioDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new Usuario(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UsuarioDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
